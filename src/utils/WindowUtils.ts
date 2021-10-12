2ea5d7470f59d73961b20ab50b74f720ad402b09
Asthenosphere <58675456+Asthenosphere@users.noreply.github.com>
2021-10-12 10:30:34 +0800

    Fix unresponsive bug after iOS swipe on mobile

---
+++ src/utils/WindowUtils.ts
@@ -0,0 +1,34 @@
+import { useState, useEffect, useCallback } from "react";
+
+export const useWindowSize = (): {
+  width: number | undefined;
+  height: number | undefined;
+  isDesktop: boolean;
+} => {
+  const isClient = typeof window === "object";
+
+  const getSize = useCallback(() => {
+    return {
+      width: isClient ? window.innerWidth : undefined,
+      height: isClient ? window.innerHeight : undefined,
+      isDesktop: isClient && window.innerWidth >= 992,
+    };
+  }, [isClient]);
+
+  const [windowSize, setWindowSize] = useState(getSize);
+
+  useEffect(() => {
+    if (!isClient) {
+      return;
+    }
+
+    const handleResize = () => {
+      setWindowSize(getSize());
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [isClient, getSize]); // Empty array ensures that effect is only run on mount and unmount
+
+  return windowSize;
+};
