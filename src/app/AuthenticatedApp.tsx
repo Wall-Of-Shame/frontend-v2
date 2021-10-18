cd361b230a094ff5abf5e36a83a9a7ffc39d4538
Asthenosphere <58675456+Asthenosphere@users.noreply.github.com>
2021-10-18 23:27:09 +0800

    Add desktop navbar

--- src/app/AuthenticatedApp.tsx
+++ src/app/AuthenticatedApp.tsx
@@ -1,4 +1,18 @@
-import { IonApp, IonRouterOutlet, IonSplitPane } from "@ionic/react";
+import {
+  IonApp,
+  IonAvatar,
+  IonButton,
+  IonCol,
+  IonIcon,
+  IonLabel,
+  IonRouterOutlet,
+  IonRow,
+  IonSplitPane,
+  IonTabBar,
+  IonTabButton,
+  IonText,
+  IonToolbar,
+} from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
 import { Route } from "react-router-dom";
 
@@ -22,22 +36,111 @@ import "@ionic/react/css/display.css";
 import "../theme/variables.scss";
 
 import "./App.scss";
-import Menu from "../components/menu";
 import Tabs from "../pages/tabs";
-import RightMenu from "../components/rightMenu";
+import RightMenu from "../components/rightSection";
 import { isPlatform } from "@ionic/core";
+import {
+  flashlightOutline,
+  storefrontOutline,
+  personOutline,
+  bugOutline,
+  hammerOutline,
+} from "ionicons/icons";
+import logo from "../assets/icon-192x192.png";
+import challengeIcon from "../assets/icons/challenge-icon.svg";
+import shameIcon from "../assets/icons/shame-icon.svg";
+import { useWindowSize } from "../utils/WindowUtils";
 
 const AuthenticatedApp: React.FC = () => {
+  const { width } = useWindowSize();
+
   return (
     <IonApp>
       <IonReactRouter>
+        {isPlatform("desktop") && (
+          <IonToolbar className='desktop-navbar'>
+            <IonRow className='ion-align-items-center'>
+              {width! >= 992 && (
+                <IonCol size='4'>
+                  <IonRow className='ion-align-items-center'>
+                    <IonAvatar
+                      style={{
+                        width: "2.5rem",
+                        height: "2.5rem",
+                        marginLeft: "1rem",
+                        marginRight: "1rem",
+                      }}
+                    >
+                      <img src={logo} alt='logo' />
+                    </IonAvatar>
+                    <IonText
+                      style={{ fontWeight: "bold", fontSize: "1.25rem" }}
+                    >
+                      Wall of Shame
+                    </IonText>
+                  </IonRow>
+                </IonCol>
+              )}
+              <IonCol
+                className='ion-no-padding'
+                size={width! >= 992 ? "4" : "12"}
+              >
+                <IonTabBar className='desktop-navbar-tabs'>
+                  <IonTabButton tab='challenges' href='/challenges'>
+                    <IonIcon icon={challengeIcon} />
+                    <IonLabel>Challenges</IonLabel>
+                  </IonTabButton>
+                  <IonTabButton tab='explore' href='/explore'>
+                    <IonIcon icon={flashlightOutline} />
+                    <IonLabel>Explore</IonLabel>
+                  </IonTabButton>
+                  <IonTabButton tab='wall-of-shame' href='/wall-of-shame'>
+                    <IonIcon icon={shameIcon} />
+                    <IonLabel>Wall</IonLabel>
+                  </IonTabButton>
+                  <IonTabButton tab='store' href='/store'>
+                    <IonIcon icon={storefrontOutline} />
+                    <IonLabel>Store</IonLabel>
+                  </IonTabButton>
+                  <IonTabButton tab='profile' href='/profile'>
+                    <IonIcon icon={personOutline} />
+                    <IonLabel>Profile</IonLabel>
+                  </IonTabButton>
+                </IonTabBar>
+              </IonCol>
+              {width! >= 992 && (
+                <IonCol size='4'>
+                  <IonRow className='ion-align-items-center ion-justify-content-end'>
+                    <IonButton mode='ios' color='clear'>
+                      <IonIcon
+                        icon={hammerOutline}
+                        color='dark'
+                        style={{ fontSize: "1.5rem" }}
+                      />
+                    </IonButton>
+                    <IonButton mode='ios' color='clear'>
+                      <IonIcon
+                        icon={bugOutline}
+                        color='dark'
+                        style={{ fontSize: "1.5rem" }}
+                      />
+                    </IonButton>
+                  </IonRow>
+                </IonCol>
+              )}
+            </IonRow>
+          </IonToolbar>
+        )}
         <IonSplitPane contentId='main' className='split-pane-main'>
-          {!isPlatform("ios") && <Menu />}
-          <IonRouterOutlet id='main' className='split-pane-content'>
+          <IonRouterOutlet
+            id='main'
+            className='split-pane-content'
+            style={{ marginTop: isPlatform("desktop") ? "3.5rem" : 0 }}
+          >
             <Route path='/challenges' render={() => <Tabs />} />
             <Route path='/' render={() => <Tabs />} />
           </IonRouterOutlet>
-          {!isPlatform("ios") && <RightMenu />}
+          {!isPlatform("iphone") && <RightMenu />}
         </IonSplitPane>
       </IonReactRouter>
     </IonApp>
