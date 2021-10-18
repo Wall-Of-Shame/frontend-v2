ed92282498b2967858e361613c00d4b55603e426
Asthenosphere <58675456+Asthenosphere@users.noreply.github.com>
2021-10-18 23:23:58 +0800

    Update right menu into a right section

---
+++ src/components/rightSection/RightSection.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import {
+  IonMenu,
+  IonContent,
+  IonList,
+  IonListHeader,
+  IonItem,
+  IonIcon,
+} from "@ionic/react";
+import { hammerOutline, bugOutline } from "ionicons/icons";
+import "./RightSection.scss";
+import FeedbackModal from "../feedback";
+import { useWindowSize } from "../../utils/WindowUtils";
+
+const RightSection = () => {
+  const { width, height } = useWindowSize();
+  const [showFeedbackModal, setShowFeedbackModal] = useState(false);
+  return (
+    <IonMenu
+      contentId='right-section-menu'
+      className='right-section'
+      side='end'
+      hidden={!(width! > 576 && height! > 576)}
+    >
+      <IonContent forceOverscroll={false} id='right-section-menu'>
+        <IonList lines='none'>
+          <IonListHeader>Help</IonListHeader>
+          <IonItem button>
+            <IonIcon slot='start' icon={hammerOutline} />
+            Tutorial
+          </IonItem>
+          <IonItem button onClick={() => setShowFeedbackModal(true)}>
+            <IonIcon slot='start' icon={bugOutline} />
+            Feedback
+          </IonItem>
+        </IonList>
+        <FeedbackModal
+          showModal={showFeedbackModal}
+          setShowModal={setShowFeedbackModal}
+        />
+      </IonContent>
+    </IonMenu>
+  );
+};
+
+export default RightSection;
