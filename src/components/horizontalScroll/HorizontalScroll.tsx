06b593b82922618dea56840c7d8d46c9bd9d7f8d
Asthenosphere <58675456+Asthenosphere@users.noreply.github.com>
2021-10-21 01:59:22 +0800

    Add horizontal scroll component

--- src/components/horizontalScroll/HorizontalScroll.tsx
+++ src/components/horizontalScroll/HorizontalScroll.tsx
@@ -1,28 +1,45 @@
-import { IonCol, IonRow } from "@ionic/react";
+import { IonAvatar, IonCol, IonRow, IonText } from "@ionic/react";
+import { UserList } from "../../interfaces/models/Users";
+import { trimDisplayName } from "../../utils/ProfileUtils";
+import AvatarImg from "../avatar";
 import "./HorizontalScroll.scss";
 
 interface HorizontalScrollProps {
-  profiles: any[];
+  users: UserList[];
 }
 
-const HorizontalScroll: React.FC<HorizontalScrollProps> = (
-  props: HorizontalScrollProps
-) => {
-  const { profiles } = props;
+const Stories = (props: HorizontalScrollProps) => {
+  const { users } = props;
 
   return (
-    <IonRow className='stories'>
-      <div className='story-container'>
-        {profiles.map((story, index) => {
+    <div>
+      <ul className='horizontal-list'>
+        {users.map((u) => {
           return (
-            <IonCol key={index} className='story'>
-              <img alt='story avatar' src={story.avatar} />
-            </IonCol>
+            <li className='horizontal-item' key={u.userId}>
+              <IonCol>
+                <IonRow className='ion-justify-content-center'>
+                  <IonAvatar>
+                    <AvatarImg avatar={u.avatar} />
+                  </IonAvatar>
+                </IonRow>
+                <IonRow className='ion-justify-content-center'>
+                  <IonText
+                    style={{
+                      fontSize: "0.75rem",
+                      marginTop: "0.5rem",
+                    }}
+                  >
+                    {trimDisplayName(u.name, 7)}
+                  </IonText>
+                </IonRow>
+              </IonCol>
+            </li>
           );
         })}
-      </div>
-    </IonRow>
+      </ul>
+    </div>
   );
 };
 
-export default HorizontalScroll;
+export default Stories;
