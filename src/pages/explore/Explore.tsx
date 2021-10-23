3a839bb6851b1ef4172a52fe399b4e7f18774b6f
jeminsieow <sjemin7@gmail.com>
2021-10-23 13:06:06 +0800

    Add search page

--- src/pages/explore/Explore.tsx
+++ src/pages/explore/Explore.tsx
@@ -17,7 +17,7 @@ import {
   IonToolbar,
 } from "@ionic/react";
 import { parseISO } from "date-fns";
-import { funnelOutline } from "ionicons/icons";
+import { search } from "ionicons/icons";
 import { format } from "path";
 import { useEffect } from "react";
 import { useLocation } from "react-router";
@@ -26,11 +26,19 @@ import Container from "../../components/container";
 import { Avatar } from "../../interfaces/models/Users";
 import { showTabs, hideTabs } from "../../utils/TabsUtils";
 import { useWindowSize } from "../../utils/WindowUtils";
+import { useChallenge } from "../../contexts/ChallengeContext";
 import "./Explore.scss";
 
 const Explore: React.FC = () => {
   const location = useLocation();
   const { isDesktop } = useWindowSize();
+  const { getAllChallenges } = useChallenge();
+
+  // const [publicChallenge]
+
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
 
   const renderChallenges = () => {
     return (
@@ -41,6 +49,181 @@ const Explore: React.FC = () => {
       </Container>
     )
   }
+
+  // const fetchData = async () => {
+  //   try {
+  //     const allChallenges = await getAllChallenges();
+  //     setOngoing(allChallenges.ongoing);
+  //     setPendingStart(allChallenges.pendingStart);
+  //     setPendingResponse(allChallenges.pendingResponse);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  // const renderChallenges = () => {
+  //   if (
+  //     (pendingResponse && pendingResponse.length > 0) ||
+  //     (pendingStart && pendingStart.length > 0)
+  //   ) {
+  //     return (
+  //       <>
+  //         {pendingResponse.length > 0 && (
+  //           <>
+  //             <IonRow className="ion-padding-horizontal ion-margin-top">
+  //               <IonText style={{ color: "gray" }}>Pending Invitations</IonText>
+  //             </IonRow>
+  //             {pendingResponse?.map((c) => {
+  //               const acceptedCount = c.participants.accepted.completed.concat(
+  //                 c.participants.accepted.notCompleted
+  //               ).length;
+  //               return (
+  //                 <IonCard
+  //                   mode="ios"
+  //                   button
+  //                   key={c.challengeId}
+  //                   onClick={() => {
+  //                     history.push(`challenges/${c.challengeId}/details`, c);
+  //                   }}
+  //                 >
+  //                   <IonGrid className="ion-no-padding">
+  //                     <IonRow className="ion-align-items-center">
+  //                       <IonCol size="12">
+  //                         <IonCardHeader style={{ paddingBottom: "0.75rem" }}>
+  //                           <IonCardTitle style={{ fontSize: "1.2rem" }}>
+  //                             {c.title}
+  //                           </IonCardTitle>
+  //                         </IonCardHeader>
+  //                         <IonCardContent>
+  //                           <IonRow>
+  //                             <IonText
+  //                               style={{
+  //                                 fontSize: "0.8rem",
+  //                                 fontWeight: "bold",
+  //                               }}
+  //                             >
+  //                               Waiting for your response
+  //                             </IonText>
+  //                           </IonRow>
+  //                           <IonRow style={{ marginTop: "0.5rem" }}>
+  //                             <IonText style={{ fontSize: "0.8rem" }}>
+  //                               {acceptedCount} participant
+  //                               {acceptedCount === 1 ? "" : "s"}
+  //                             </IonText>
+  //                           </IonRow>
+  //                           <IonRow
+  //                             style={{ marginTop: "0.5rem" }}
+  //                             className="ion-align-items-center"
+  //                           >
+  //                             <IonAvatar
+  //                               className="avatar"
+  //                               key={c.owner.userId}
+  //                               style={{ marginRight: "0.5rem" }}
+  //                             >
+  //                               <AvatarImg avatar={c.owner.avatar as Avatar} />
+  //                             </IonAvatar>
+  //                             <IonText
+  //                               style={{
+  //                                 fontSize: "0.8rem",
+  //                               }}
+  //                             >
+  //                               Created by {c.owner.name ?? "Anonymous"}
+  //                             </IonText>
+  //                           </IonRow>
+  //                         </IonCardContent>
+  //                       </IonCol>
+  //                     </IonRow>
+  //                   </IonGrid>
+  //                 </IonCard>
+  //               );
+  //             })}
+  //           </>
+  //         )}
+  //         {pendingStart.length > 0 && (
+  //           <>
+  //             {pendingStart?.map((c) => {
+  //               const acceptedCount = c.participants.accepted.completed.concat(
+  //                 c.participants.accepted.notCompleted
+  //               ).length;
+  //               return (
+  //                 <IonCard
+  //                   mode="ios"
+  //                   button
+  //                   key={c.challengeId}
+  //                   onClick={() => {
+  //                     history.push(`challenges/${c.challengeId}/details`, c);
+  //                   }}
+  //                 >
+  //                   <IonGrid className="ion-no-padding">
+  //                     <IonRow className="ion-align-items-center">
+  //                       <IonCol size="12">
+  //                         <IonCardHeader style={{ paddingBottom: "0.75rem" }}>
+  //                           <IonCardTitle style={{ fontSize: "1.2rem" }}>
+  //                             {c.title}
+  //                           </IonCardTitle>
+  //                         </IonCardHeader>
+  //                         <IonCardContent>
+  //                           <IonRow>
+  //                             <IonText
+  //                               style={{
+  //                                 fontSize: "0.8rem",
+  //                                 fontWeight: "bold",
+  //                               }}
+  //                             >
+  //                               {`Starts on ${format(
+  //                                 parseISO(c.startAt!),
+  //                                 "dd MMM yyyy, HH:mm"
+  //                               )}`}
+  //                             </IonText>
+  //                           </IonRow>
+  //                           <IonRow style={{ marginTop: "0.5rem" }}>
+  //                             <IonText style={{ fontSize: "0.8rem" }}>
+  //                               {acceptedCount} participant
+  //                               {acceptedCount === 1 ? " has " : "s have "}
+  //                               accepted
+  //                             </IonText>
+  //                           </IonRow>
+  //                           <IonRow
+  //                             style={{ marginTop: "0.5rem" }}
+  //                             className="ion-align-items-center"
+  //                           >
+  //                             <IonAvatar
+  //                               className="avatar"
+  //                               key={c.owner.userId}
+  //                               style={{ marginRight: "0.5rem" }}
+  //                             >
+  //                               <AvatarImg avatar={c.owner.avatar as Avatar} />
+  //                             </IonAvatar>
+  //                             <IonText
+  //                               style={{
+  //                                 fontSize: "0.8rem",
+  //                               }}
+  //                             >
+  //                               Created by {c.owner.name ?? "Anonymous"}
+  //                             </IonText>
+  //                           </IonRow>
+  //                         </IonCardContent>
+  //                       </IonCol>
+  //                     </IonRow>
+  //                   </IonGrid>
+  //                 </IonCard>
+  //               );
+  //             })}
+  //           </>
+  //         )}
+  //       </>
+  //     );
+  //   } else {
+  //     return (
+  //       <IonRow
+  //         className="ion-padding ion-justify-content-center"
+  //         style={{ marginTop: "1.5rem" }}
+  //       >
+  //         {"There's nothing here >_<"}
+  //       </IonRow>
+  //     );
+  //   }
+  // };
 
   useEffect(() => {
     if (
@@ -77,19 +260,18 @@ const Explore: React.FC = () => {
           </IonTitle>
           <IonFabButton
             className="placeholder-fab"
-            color="clear"
+            color="main-blue"
             mode="ios"
             slot="end"
-            disabled
             style={{
               margin: "0.5rem",
               width: "2.75rem",
               height: "2.75rem",
             }}
+            routerLink='/explore/search'
           >
             <IonIcon
-              icon={funnelOutline}
-              color="main-blue"
+              icon={search}
               style={{ fontSize: "1.5rem" }}
             />
           </IonFabButton>
