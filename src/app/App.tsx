e3d917ca7fb58ff2feaca99d351a8d3870f5ac06
Asthenosphere <58675456+Asthenosphere@users.noreply.github.com>
2021-10-27 10:43:57 +0800

    Remove browser check

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import LoadingSpinner from "../components/loadingSpinner/LoadingSpinner";
 import { useUser } from "../contexts/UserContext";
 import AuthenticatedApp from "./AuthenticatedApp";
 import UnauthenticatedApp from "./UnauthenticatedApp";
 import "./App.scss";
-import Alert from "../components/alert";
 
 const App: React.FC = () => {
   const user = useUser();
-  const [showBrowserAlert, setShowBroserAlert] = useState(false);
-
-  useEffect(() => {
-    const userAgent = window.navigator.userAgent;
-    if (
-      userAgent.indexOf("Chrome") === -1 &&
-      window.localStorage.getItem("browserWarning") !== "true"
-    ) {
-      setShowBroserAlert(true);
-      window.localStorage.setItem("browserWarning", "true");
-    }
-  }, []);
 
   return (
     <React.Suspense
@@ -32,14 +19,6 @@ const App: React.FC = () => {
       }
     >
       {user.user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
-      <Alert
-        showAlert={showBrowserAlert}
-        alertHeader={"Notice"}
-        alertMessage={"This app is best optimised for Google Chrome :)"}
-        hasConfirm={false}
-        closeAlert={() => setShowBroserAlert(false)}
-        confirmHandler={() => {}}
-      />
     </React.Suspense>
   );
 };
