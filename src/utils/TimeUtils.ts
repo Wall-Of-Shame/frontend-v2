8f2ca4fc20af2dbee19ca7a30f1409fe57c06e56
Asthenosphere <58675456+Asthenosphere@users.noreply.github.com>
2021-10-29 01:30:25 +0800

    Fix singular plural on wall

--- src/utils/TimeUtils.ts
+++ src/utils/TimeUtils.ts
@@ -1,17 +1,19 @@
 export const formatWallTime = (duration: Duration): string => {
   if (duration.years && duration.years > 0) {
-    return `${duration.years} years`;
+    return `${duration.years} year${duration.years === 1 ? "" : "s"}`;
   } else if (duration.months && duration.months > 0) {
-    return `${duration.months} months`;
+    return `${duration.months} month${duration.months === 1 ? "" : "s"}`;
   } else if (duration.weeks && duration.weeks > 0) {
-    return `${duration.weeks} weeks`;
+    return `${duration.weeks} week${duration.weeks === 1 ? "" : "s"}`;
   } else if (duration.days && duration.days > 0) {
-    return `${duration.days} days`;
+    return `${duration.days} day${duration.days === 1 ? "" : "s"}`;
   } else if (duration.hours && duration.hours > 0) {
-    return `${duration.hours} hours`;
+    return `${duration.hours} hour${duration.hours === 1 ? "" : "s"}`;
   } else if (duration.minutes && duration.minutes > 0) {
-    return `${duration.minutes} minutes`;
+    return `${duration.minutes} minute${duration.minutes === 1 ? "" : "s"}`;
   } else {
-    return `${duration.seconds ?? 0} seconds`;
+    return `${duration.seconds ?? 0} second${
+      duration.seconds === 1 ? "" : "s"
+    }`;
   }
 };
