a5314fdf25714e05dec255efb7f3f29aa71d865f
jiefeng <lowjiefeng1998@gmail.com>
2021-11-05 18:46:59 +0800

    Add loading while getting firebase redirect result

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,35 +1,16 @@
-import React, { useEffect, useState } from "react";
-import { getRedirectResult } from "firebase/auth";
+import React, { useState } from "react";
 import LoadingSpinner from "../components/loadingSpinner/LoadingSpinner";
 import { useUser } from "../contexts/UserContext";
 import { useCache } from "../contexts/CacheContext";
 import AuthenticatedApp from "./AuthenticatedApp";
 import UnauthenticatedApp from "./UnauthenticatedApp";
 import "./App.scss";
-import { auth } from "../firebase";
-import AuthService from "../services/AuthService";
-import { isInstagramBrowser } from "../utils/BrowserUtils";
 import Alert from "../components/alert";
 
 const App: React.FC = () => {
   const user = useUser();
   const { isLatestVersion, refreshCacheAndReload } = useCache();
   const [isAlertVisible, setIsAlertVisible] = useState(true);
-
-  useEffect(() => {
-    if (!isInstagramBrowser()) {
-      // skip if not instagram browser
-      return;
-    }
-    getRedirectResult(auth).then(async user => {
-      if (!user) {
-        return;
-      }
-      const token = await user.user.getIdToken();
-      await AuthService.login(token);
-      window.location.href = '';
-    });
-  }, []);
 
   return (
     <React.Suspense
