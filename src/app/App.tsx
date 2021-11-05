62e96ccf62f915a56f1c12b425228b834e3cb48a
Asthenosphere <58675456+Asthenosphere@users.noreply.github.com>
2021-11-05 15:18:57 +0800

    Add cache busting alert

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import LoadingSpinner from "../components/loadingSpinner/LoadingSpinner";
 import { useUser } from "../contexts/UserContext";
+import { useCache } from "../contexts/CacheContext";
 import AuthenticatedApp from "./AuthenticatedApp";
 import UnauthenticatedApp from "./UnauthenticatedApp";
 import "./App.scss";
+import Alert from "../components/alert";
 
 const App: React.FC = () => {
   const user = useUser();
+  const { isLatestVersion, refreshCacheAndReload } = useCache();
+  const [isAlertVisible, setIsAlertVisible] = useState(true);
 
   return (
     <React.Suspense
@@ -19,6 +23,22 @@ const App: React.FC = () => {
       }
     >
       {user.user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      {!isLatestVersion && (
+        <Alert
+          showAlert={isAlertVisible}
+          closeAlert={() => {
+            setIsAlertVisible(false);
+            refreshCacheAndReload();
+          }}
+          alertHeader={"App Update"}
+          alertMessage={
+            "A new version of the app is now available! This update will only take a few seconds."
+          }
+          hasConfirm={false}
+          confirmHandler={() => {}}
+          okHandler={refreshCacheAndReload}
+        />
+      )}
     </React.Suspense>
   );
 };
